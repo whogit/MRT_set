8d6220ab20c25403bff88701d75a1bb282744f07
who <wilbur.hurst.otte@gmail.com>
2015-10-20 19:53:51 -0400

    MonitorReports group

---
+++ ReportLibrary/ReportLibrary/Reporter.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ReportLibrary
+{
+    public class Reporter
+    {
+        public static void ReportOSVersion()
+        {
+            string os_string = Environment.OSVersion.Version.ToString();
+            if (os_string.Contains("5.1.2600") == true)
+            {
+                Console.WriteLine("WinXP");
+            }
+            else if (os_string.Contains("6.1.7600") == true)           
+            {    
+                 Console.WriteLine("Win7");
+            }
+            else
+            {
+                Console.WriteLine("Fall Through:");
+                Console.WriteLine(os_string);
+            }                        
+        }
+        public static bool Is64() { return IntPtr.Size == 8 ? true : false; }  
+
+        public static void ReportOSBitVersion()
+        {                        
+            if (Is64())
+            {
+                Console.WriteLine("x64");
+            }
+            else
+            {                
+                Console.WriteLine("x86");
+            }                         
+        }        
+    }
+}
---
+++ ToolLibrary/ToolLibrary/LogTools.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+
+namespace ToolLibrary
+{
+    public class LogTools
+    {
+
+        public static void ClearLog(string Log)
+        {
+            foreach (var eventLog in EventLog.GetEventLogs())
+            {
+                // Console.WriteLine(eventLog.LogDisplayName);
+                if (eventLog.LogDisplayName == Log)
+                {
+                    Console.WriteLine("I found " + eventLog.LogDisplayName);
+                    Console.WriteLine("Clearing Log...");
+                    eventLog.Clear();
+                }
+            }
+        }
+        public static void MakeLogEntry()
+        {
+            // It is quite easy to write to the Application
+            // log but much more involved to write to the
+            // System log.  I prefer not to mess with the
+            // System log at this time.
+            string sSource;
+            string sLog;
+            string sEvent;
+
+            sSource = "dotNET Sample App";
+            sLog = "Application";
+            sEvent = "Sample Event";
+
+            if (!EventLog.SourceExists(sSource))
+            {
+                EventLog.CreateEventSource(sSource, sLog);
+            }
+
+            EventLog.WriteEntry(sSource, sEvent);
+            EventLog.WriteEntry(sSource, sEvent, EventLogEntryType.Warning, 234);
+        }
+    }
+
+}
